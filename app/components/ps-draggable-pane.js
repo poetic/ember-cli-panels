f91f5a7338762fb43e89de77bd8e9dca666bd7e1
Jake Craige <jake@poeticsystems.com>
2014-12-12 12:07:09 -0600

    refactor ps-draggable to various methods

--- app/components/ps-draggable-pane.js
+++ app/components/ps-draggable-pane.js
@@ -1,6 +1,214 @@
 import Ember from 'ember';
 import PsPane from './ps-pane';
 
+export default PsPane.extend({
+  reverseCss:        Ember.A([]),
+  $container:        null,
+  $panel:            null,
+
+  panes: Ember.computed.alias('panel.renderedPanes'),
+
+  paneIndex: Ember.computed('panes.[]', function() {
+    return this.get('panes').indexOf(this);
+  }),
+
+  prevPane: Ember.computed('panes.[]', 'paneIndex', function() {
+    return this.get('panes')[this.get('paneIndex') - 1]
+  }),
+
+  nextPane: Ember.computed('panes.[]', 'paneIndex', function() {
+    return this.get('panes')[this.get('paneIndex') + 1]
+  }),
+
+  threshold: Ember.computed('elWidth', function() {
+    return this.get('elWidth') * 0.30
+  }),
+
+  setupHammer: Ember.on('didInsertElement', function() {
+    var component  = this;
+
+    var hammer = new Hammer(this.$()[0]);
+
+    hammer.get('pan').set({
+      direction: Hammer.DIRECTION_HORIZONTAL
+    });
+
+    hammer.on('panstart', Ember.run.bind(this, this.panstart));
+    hammer.on('panmove', Ember.run.bind(this, this.panmove));
+    hammer.on('panend', Ember.run.bind(this, this.panend));
+  }),
+
+  setupJqCache: Ember.on('didInsertElement', function() {
+    var $panel = this.get('panel').$();
+    this.set('$panel', $panel);
+    this.set('$container', $panel.find('.ps-panel-container'));
+  }),
+
+  panstart: function(/* event */) {
+    if (this.get('animating')) {
+      return;
+    }
+
+    this.setProperties({
+      reverseCss: []
+    });
+
+    var component    = this;
+    var $el          = this.$();
+    var $panel       = this.get('$panel');
+    var maxHeight    = this.calculateMaxPaneHeight();
+    var elWidth      = $el.outerWidth();
+    var elHeight     = $el.outerHeight();
+    var elPosition   = $el.position();
+    var $currentPane = this.$();
+
+    var cssOpts = {
+      position:  'absolute',
+      top:       elPosition.top,
+      left:      elPosition.left,
+      width:     elWidth,
+      height:    elHeight
+    };
+
+    this.set('elWidth', elWidth);
+
+    this.setCss($panel, { height: maxHeight })
+
+    cssOpts.left = (elPosition.left - elWidth) + 'px';
+    this.showAndSetCss(this.get('prevPane'), cssOpts);
+
+    cssOpts.left = (elPosition.left + elWidth) + 'px';
+    this.showAndSetCss(this.get('nextPane'), cssOpts);
+  },
+
+  panmove: function(event) {
+    if (this.get('animating')) {
+      return;
+    }
+
+    Ember.$.Velocity(this.get('$container'), {
+      translateX: event.deltaX
+    }, { duration: 0 });
+  },
+
+  panend: function(event) {
+    if (this.get('animating')) {
+      return;
+    }
+
+    var delta     = event.deltaX;
+    var elWidth   = this.get('elWidth');
+    var threshold = this.get('threshold');
+    var prevPane  = this.get('prevPane');
+    var nextPane  = this.get('nextPane');
+
+    var animation;
+
+    if (prevPane && delta > threshold) {
+      animation = this.animateToNewPane(elWidth, prevPane);
+
+    } else if (delta > 0) {
+      animation = this.animateToCurrentPane();
+
+    } else if (nextPane && Math.abs(delta) > threshold) {
+      animation = this.animateToNewPane(-elWidth, nextPane);
+
+    } else {
+      animation = this.animateToCurrentPane();
+
+    }
+
+    return this.finishAnimation(animation);
+  },
+
+  animateToNewPane: function(translateX, pane) {
+    var component = this;
+    this.set('animating', true);
+
+    return animate(this.get('$container'), {
+      translateX: translateX
+    }).then(function() {
+      component.set('panel.currentPane', pane.get('name'));
+    });
+  },
+
+  animateToCurrentPane: function() {
+    var component = this;
+    component.set('animating', true);
+
+    return animate(this.get('$container'), {
+      translateX: 0
+    }).then(function() {
+      return component.get('panel').updateVisiblePane();
+    });
+  },
+
+  finishAnimation: function(animation) {
+    var component = this;
+
+    return Ember.RSVP.resolve(animation).then(function() {
+      component.get('reverseCss').forEach(function(reverseFn) {
+        return reverseFn();
+      });
+
+      component.get('$container').css({
+        transform: 'translateX(0px)'
+      });
+    }).then(function() {
+      component.set('animating', false);
+    });
+  },
+
+  showAndSetCss: function(el, cssOpts) {
+    if (!el) {
+      return;
+    }
+
+    var $el = el.$();
+
+    this.setCss($el, cssOpts);
+    $el.removeClass('hide');
+  },
+
+  // We need to set the height to the max height out of ALL the panes. If
+  // it's shorter than one, it will look cut off during the drag
+  //
+  // We also do this hacky one time cache because it takes ~45ms to
+  // calculate this and causes the drag to lag
+  calculateMaxPaneHeight: function() {
+    var maxHeight = this.get('maxHeight');
+
+    if (!maxHeight) {
+      maxHeight = this.get('panes').reduce(function(height, pane) {
+        var paneHeight = pane.$().outerHeight();
+
+        if (paneHeight > height) {
+          return paneHeight;
+        }
+
+        return height;
+      }, 0);
+
+      this.set('maxHeight', maxHeight);
+    }
+
+    return maxHeight;
+  },
+
+  setCss: function($el, opts) {
+    var reverseFn = setCss($el, opts);
+    return this.get('reverseCss').pushObject(reverseFn);
+  },
+
+  _hideAnimation: function() {
+    return this.$().addClass('hide');
+  },
+
+  _showAnimation: function() {
+    return this.$().removeClass('hide');
+  }
+});
+
 function animate($el, opts) {
   return Ember.$.Velocity($el, opts, 375, [400, 25]);
 }
@@ -19,184 +227,3 @@ function setCss($el, cssOpts) {
   };
 }
 
-export default PsPane.extend({
-  setupTouch: Ember.on('didInsertElement', function() {
-    var component  = this;
-    var $el        = this.$();
-    var $panel     = this.$().parents('.ps-panel');
-    var $container = this.$().parents('.ps-panel-container');
-
-    var hammer = new Hammer($el[0]);
-
-    hammer.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL });
-    hammer.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
-
-    var panning = false;
-
-    var elWidth, elHeight, elPosition, visiblePaneOpts, panes, currentPaneIndex,
-        prevPane, currentPane, nextPane, offset, reverseCss = [];
-
-    var animateToNextPane = function() {
-      return animate($container, {
-        translateX: -elWidth
-      }).then(function() {
-        component.set('panel.currentPane', nextPane.get('name'));
-      });
-    };
-
-    var animateToPrevPane = function() {
-      return animate($container, {
-        translateX: elWidth
-      }).then(function() {
-        component.set('panel.currentPane', prevPane.get('name'));
-      });
-    };
-
-    var animateToCurrentPane = function() {
-      return animate($container, { translateX: 0 }).then(function() {
-        return component.get('panel').updateVisiblePane();
-      });
-    };
-
-    var finishAnimation = function(animation) {
-      panning = false;
-
-      return Ember.RSVP.resolve(animation).then(function() {
-        reverseCss.forEach(function(fn) { fn() });
-        $container.css({transform: 'translateX(0px)'});
-      });
-    }
-
-    hammer.on('panstart', function() {
-      panning = true;
-      reverseCss = [];
-
-      panes            = component.get('panel.renderedPanes');
-      currentPaneIndex = panes.indexOf(component);
-
-      // We need to set the height to the max height out of ALL the panes. If
-      // it's shorter than one, it will look cut off during the drag
-      //
-      // We also do this hacky one time cache because it takes ~45ms to
-      // calculate this and causes the drag to lag
-      var maxHeight = component.get('maxHeight');
-      if (!maxHeight) {
-        maxHeight = panes.reduce(function(height, pane) {
-          var paneHeight = pane.$().outerHeight();
-
-          if (paneHeight > height) {
-            return paneHeight;
-          }
-
-          return height;
-        }, 0);
-        component.set('maxHeight', maxHeight);
-      }
-
-      reverseCss.push(
-        setCss($panel, { height: maxHeight })
-      );
-
-      elWidth    = $el.outerWidth();
-      elHeight   = $el.outerHeight();
-      elPosition = $el.position();
-
-      offset = elWidth * 0.30;
-
-      visiblePaneOpts = {
-        position:  'absolute',
-        top:       elPosition.top,
-        left:      elPosition.left,
-        width:     elWidth,
-        height:    elHeight
-      };
-
-      prevPane    = panes[currentPaneIndex - 1];
-      currentPane = component;
-      nextPane    = panes[currentPaneIndex + 1];
-
-      reverseCss.push(
-        setCss(currentPane.$(), visiblePaneOpts)
-      );
-
-      if (prevPane) {
-        visiblePaneOpts.left = (elPosition.left - elWidth) + 'px';
-        reverseCss.push(
-          setCss(prevPane.$(), visiblePaneOpts)
-        );
-        prevPane.$().removeClass('hide');
-      }
-
-      if (nextPane) {
-        visiblePaneOpts.left = (elPosition.left + elWidth) + 'px';
-        reverseCss.push(
-          setCss(nextPane.$(), visiblePaneOpts)
-        );
-        nextPane.$().removeClass('hide');
-      }
-    });
-
-    hammer.on('panmove', function(event) {
-      if (!panning) {
-        return;
-      }
-
-      Ember.$.Velocity($container, {
-        translateX: event.deltaX
-      }, { duration: 0 });
-    });
-
-    hammer.on('panend', function(event) {
-      if (!panning) {
-        return;
-      }
-
-      var delta = event.deltaX;
-
-      var animation;
-
-      if (prevPane && delta > offset) {
-        animation = animateToPrevPane();
-      } else if (delta > 0) {
-        animation = animateToCurrentPane();
-      } else if (nextPane && Math.abs(delta) > offset) {
-        animation = animateToNextPane();
-      } else {
-        animation = animateToCurrentPane();
-      }
-
-      finishAnimation(animation);
-    });
-
-    // hammer.on('swipeleft', function() {
-      // var animation;
-
-      // if (nextPane) {
-        // animation = animateToNextPane();
-      // } else {
-        // animation = animateToCurrentPane();
-      // }
-
-      // finishAnimation(animation);
-    // });
-
-    // hammer.on('swiperight', function() {
-      // var animation;
-      // if (prevPane) {
-        // animation = animateToPrevPane();
-      // } else {
-        // animation = animateToCurrentPane();
-      // }
-
-      // finishAnimation(animation);
-    // });
-  }),
-
-  _hideAnimation: function() {
-    return this.$().addClass('hide');
-  },
-
-  _showAnimation: function() {
-    return this.$().removeClass('hide');
-  }
-});
--- app/components/ps-pane.js
+++ app/components/ps-pane.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   classNames: 'ps-pane',
 
   panel: Ember.computed.alias('parentView'),
+  animating: Ember.computed.alias('panel.animating'),
 
   registerPane: Ember.on('didInsertElement', function() {
     return this.get('panel').registerPane(this);
